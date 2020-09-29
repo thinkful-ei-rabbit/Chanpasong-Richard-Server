ec4570f645965c1e556a9e0f3e2179f4c9dc08c7
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2020-09-29 11:49:50 -0500

    added test starts and bearertoken validation

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const notesRouter = require("./notes/notes-router");
 const folderRouter = require("./folders/folder-router");
+const validateBearer = require("./validateBearer");
+const errorHandler = require("./errorHandler");
 
 const app = express();
 
@@ -15,21 +17,11 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(validateBearer);
 
 app.use("/notes", notesRouter);
 app.use("/folders", folderRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+app.use(errorHandler);
+
 module.exports = app;
---
+++ src/validateBearer.js
@@ -0,0 +1,15 @@
+const { API_TOKEN } = require('./config');
+
+
+function validateBearerToken(req, res, next) {
+  const authToken = req.get('Authorization');
+  console.log(API_TOKEN);
+  console.log(authToken);
+  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+
+  next();
+}
+
+module.exports = validateBearerToken;
